f78ec84dc6dd35dabec530f5dcb2f2baa9ac5ca0
Vlad <juzopigeon@gmail.com>
2021-11-12 09:28:05 +0600

    Create EventsExample.tsx

---
+++ src/components/EventsExample.tsx
@@ -0,0 +1,53 @@
+import React, { FC, useRef, useState } from 'react'
+
+const EventsExample:FC = () => {
+  const [value, setValue] = useState<string>('')
+  const [isDrag, setIsDrag] = useState<boolean>(false)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+   const changeHandler = (e:   React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value)
+   }
+
+   const clickHandler = (e: React.MouseEvent<HTMLButtonElement>)  => {
+      console.log(inputRef.current?.value)
+   }
+
+   const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
+     console.log('drag')
+   }
+
+   const dragWithPreventHandler = (e: React.DragEvent<HTMLDivElement>) =>  {
+     e.preventDefault()
+     setIsDrag(true)
+   }
+
+   const leaveHandler = (e: React.DragEvent<HTMLDivElement>) =>  {
+    e.preventDefault()
+    setIsDrag(false)
+  }
+
+  const dropHandler = (e: React.DragEvent<HTMLDivElement>) =>  {
+    e.preventDefault()
+    setIsDrag(false)
+    console.log('DROP')
+  }
+
+
+  return (
+    <div>
+          <input type="text" value={value} onChange={ changeHandler} placeholder="Управляемый"/>
+          <input type="text" ref={inputRef} placeholder="НеУправляемый"/>
+          <button onClick={clickHandler}>fesafsaf</button>
+          <div onDrag={dragHandler} draggable style={{width: '200px', height: '200px', background: isDrag ? 'blue' :  'red'}}></div>
+   
+          <div 
+          onDrop={dropHandler} 
+          onDragLeave={leaveHandler}
+           onDragOver={dragWithPreventHandler}
+           style={{width: '200px', height: '200px', background: 'red', marginTop: 15}}></div>
+    </div>
+  )
+}
+
+export default EventsExample
